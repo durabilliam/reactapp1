cb24fcfb997d8cd8440915e43f44717f58219f4a
William Perry <durabilliam@gmail.com>
2021-08-18 12:01:33 -0230

    add API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ContactCard from "./components/contactCard";
 
 const App = () => {
-  const contacts = [
-    { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
-    { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
-    { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
-    { name: "Scott Summers", email: "cyclops@greymalkin.com", age: 35 }
-  ];
+  const [contacts, setContacts] = useState([]);
+  // const contacts = [
+  //   { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
+  //   { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
+  //   { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
+  //   { name: "Scott Summers", email: "cyclops@greymalkin.com", age: 35 }
+  // ];
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setContacts(data.results);
+      });
+  }, [])
+
 
   const message = "No way LOL!!!!";
   const handleClick = () => {
@@ -15,14 +25,14 @@ const App = () => {
   }
   return (
     <>
-    {contacts.map(contact => (
-      <ContactCard
-      avatar="https://via.placeholder.com/150"
-      name={contact.name}
-      email={contact.email}
-      age={contact.age}
-      />
-    ))}
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
     </>
   );
 
